b70fdd60452f1631fc2a08b5f73a0468eb22e1f7
YOGEESH K P <yogeesh793@gmail.com>
2024-08-22 19:37:03 +0530

    Add files via upload

---
+++ variables_1.py
@@ -0,0 +1,13 @@
+# Variables
+age = 30                     # integer
+user_name = "yoge"           # string
+is_accepted_term = True      # bool
+Purchasing_bill = 324.24     # float
+print(age)
+print(type(age))
+print(user_name)
+print(type(user_name))
+print(is_accepted_term)
+print(type(is_accepted_term))
+print(Purchasing_bill)
+print(type(Purchasing_bill))
